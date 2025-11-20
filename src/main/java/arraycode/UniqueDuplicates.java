3a25883c30db243d76a64aaf097a123a64eadec9
Manoj18121812 <gavhaneam@gmail.com>
2025-11-20 21:50:35 +0530

    pratice code

--- src/main/java/arraycode/UniqueDuplicates.java
+++ src/main/java/arraycode/UniqueDuplicates.java
@@ -7,7 +7,7 @@ public class UniqueDuplicates {
     public static void main(String[] args) {
 
 
-        int arr[] = {1, 2, 3, 4, 4, 5, 6, 6, 7, 8, 9};
+        int arr[] = {1, 2, 3, 4, 4, 5, 6, 6, 6,7, 8, 9};
 
         HashSet<Integer> seen = new HashSet<>();
         HashSet<Integer> duplicates = new HashSet<>();
