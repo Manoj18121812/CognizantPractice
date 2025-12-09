bc8366d9c16e1e9acbe438e06baa5aa87d88b249
Manoj Gavhane <131341403+Manoj18121812@users.noreply.github.com>
2025-12-09 17:03:00 +0530

    swap number without third variable code

---
+++ src/main/java/Number/SwapWithoutThird.java
@@ -0,0 +1,18 @@
+package Number;
+
+public class SwapWithoutThird {
+    public static void main(String[] args) {
+
+        int a=10;
+        int b=20;
+
+        System.out.println("before "+a);
+        System.out.println("before "+b);
+        a= a+b;
+        b=a-b;
+        a=a-b;
+
+        System.out.println("after "+a);
+        System.out.println("after "+b);
+    }
+}
