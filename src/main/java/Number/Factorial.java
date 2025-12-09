cf75b3a043da561037c1935dd8e381c5edf4465c
Manoj Gavhane <131341403+Manoj18121812@users.noreply.github.com>
2025-12-09 18:15:42 +0530

    factorial code code

---
+++ src/main/java/Number/Factorial.java
@@ -0,0 +1,14 @@
+package Number;
+
+public class Factorial {
+
+    public static void main(String[] args) {
+
+        int num =6;
+        long fact =1;
+        for(int i=1;i<=num;i++){
+            fact= fact*i;
+        }
+        System.out.println(fact);
+    }
+}
