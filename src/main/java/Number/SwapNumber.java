79346f66507dee8188cf80d06ec976ca5ae1942b
Manoj Gavhane <131341403+Manoj18121812@users.noreply.github.com>
2025-12-09 17:00:34 +0530

    swap number code

---
+++ src/main/java/Number/SwapNumber.java
@@ -0,0 +1,17 @@
+package Number;
+
+public class SwapNumber {
+    public static void main(String[] args) {
+
+        int a=10;
+        int b=20;
+        System.out.println("before " + a);
+        System.out.println("before " + b);
+        int temp =a;
+        a=b;
+        b=temp;
+
+        System.out.println("after "+a);
+        System.out.println("after "+b);
+    }
+}
